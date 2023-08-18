f580129a96e6c0a8297e85390da1d00621d4f2c6
sekar N <sekarnagarajan1234@gmail.com>
2023-08-18 15:44:45 +0530

    java

---
+++ Program17.java
@@ -0,0 +1,12 @@
+//17. Find the odd or even number without using odd or even.
+class Program17 {
+    public static void main(String[] args) {
+        int num = 10;
+
+        if((num/2)*2==num){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
