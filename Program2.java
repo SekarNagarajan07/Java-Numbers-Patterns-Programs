9e572c2c5bd1666a1111a59e7fd7292c2855d51b
sekar N <sekarnagarajan1234@gmail.com>
2023-08-18 13:58:30 +0530

    java

---
+++ Program2.java
@@ -0,0 +1,8 @@
+//2. Reverse the number?
+class Program2 {
+    public static void main(String[] args) {
+        for (int i = 10;i>=1;i--){
+            System.out.print(i + " ");
+        }
+    }
+}
