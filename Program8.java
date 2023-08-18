d91734bacaf18a548d1c1d772f229d8c22158633
sekar N <sekarnagarajan1234@gmail.com>
2023-08-18 14:43:07 +0530

    java

---
+++ Program8.java
@@ -0,0 +1,11 @@
+//8. Check whether the given number is positive or not ?
+class Program8 {
+    public static void main(String[] args) {
+        int num = -10;
+        if(num > 1){
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative");
+        }
+    }
+}
